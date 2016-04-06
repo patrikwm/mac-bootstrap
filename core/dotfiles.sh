b2cfb54ac2ba2bee0f187a179578ff70567099bb
Patrik Wigur Martin <patrik.martin@mideye.com>
2016-04-06 13:26:00 +0200

    add script for creating links to dotifles folder. that i sync between computers.

---
+++ core/dotfiles.sh
@@ -0,0 +1,13 @@
+#To sync dotfiles with sideroak
+
+#Vim dotfiles
+ln -s ~/dotifles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim ~/.vim
+
+#ssh folder
+ln -s ~/dotfiles/.ssh ~/.ssh
+
+#bash settings
+
+ln -s ~/dotfiles/.bash_history ~/.bash_history
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
